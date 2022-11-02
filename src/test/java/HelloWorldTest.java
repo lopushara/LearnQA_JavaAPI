13d29b54a0cf5bcc72395bf4610b82fd4ef1e88d
lopuh <G@rut0U2um@k1>
2022-11-02 15:20:34 +0300

    Hello From Alexey

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,5 +1,3 @@
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 
@@ -7,9 +5,6 @@ public class HelloWorldTest {
 
     @Test
     public void testHelloWorld(){
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/hello")
-                .andReturn();
-        response.prettyPrint();
+        System.out.println("Hello from Alexey");
     }
 }
