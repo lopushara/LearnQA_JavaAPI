716195263bac66c9dc89e96dfe9aee0cdef856e3
lopuh <G@rut0U2um@k1>
2022-11-14 14:41:06 +0300

    Added ex7 resolution

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -7,23 +7,35 @@ import java.util.Map;
 
 
 public class HelloWorldTest {
-
+    static int statusCode;
+    static String uri = "https://playground.learnqa.ru/api/long_redirect";
     @Test
     public void testRestAssured(){
         Map<String, String> headers = new HashMap<>();
         headers.put("myHeader1", "myValue1");
         headers.put("myHeader2", "myValue2");
 
-        Response response = RestAssured
-                .given()
-                .redirects()
-                .follow(false)
-                .headers(headers)
-                .when()
-                .get("https://playground.learnqa.ru/api/long_redirect")
-                .andReturn();
-        String locationHeader = response.getHeader("Location");
-        System.out.println(locationHeader);
-    }
+         do {
 
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .headers(headers)
+                    .when()
+                    .get(uri)
+                    .andReturn();
+            statusCode = response.getStatusCode();
+            System.out.println(uri);
+            System.out.println(statusCode);
+            if (response.getHeader("Location") != null) {
+                uri = response.getHeader("Location");
+            }
+            else {
+                return;
+            }
+        }
+         while (statusCode != 200);
+
+    }
 }
