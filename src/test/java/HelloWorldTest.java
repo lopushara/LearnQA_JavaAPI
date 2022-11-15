ca7426cab0f995a3004dc39d93aaaca6082e6f67
lopuh <G@rut0U2um@k1>
2022-11-15 20:40:37 +0300

    Added ex8 resolution

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,41 +1,49 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+
 import java.util.HashMap;
 import java.util.Map;
+import java.time.*;
 
 
 public class HelloWorldTest {
-    static int statusCode;
-    static String uri = "https://playground.learnqa.ru/api/long_redirect";
+    static String uri = "https://playground.learnqa.ru/ajax/api/longtime_job";
     @Test
-    public void testRestAssured(){
+    public void testRestAssured() throws InterruptedException {
         Map<String, String> headers = new HashMap<>();
-        headers.put("myHeader1", "myValue1");
-        headers.put("myHeader2", "myValue2");
 
-         do {
+        JsonPath response = RestAssured
+                .given()
+                .get(uri)
+                .jsonPath();
+        String token = response.get("token");
+        int delay = response.get("seconds");
+        System.out.println("Task will be finished in "+delay+" seconds");
+        headers.put("token", token);
 
-            Response response = RestAssured
-                    .given()
-                    .redirects()
-                    .follow(false)
-                    .headers(headers)
-                    .when()
-                    .get(uri)
-                    .andReturn();
-            statusCode = response.getStatusCode();
-            System.out.println(uri);
-            System.out.println(statusCode);
-            if (response.getHeader("Location") != null) {
-                uri = response.getHeader("Location");
-            }
-            else {
-                return;
-            }
-        }
-         while (statusCode != 200);
+        JsonPath statusCheck = RestAssured
+                .given()
+                .queryParams(headers)
+                .headers(headers)
+                .get(uri)
+                .jsonPath();
+        String status = statusCheck.get("status");
+        System.out.println("Check status Job = "+status);
+        Thread.sleep(delay*1000);
+
+        JsonPath getResult = RestAssured
+                .given()
+                .queryParams(headers)
+                .get(uri)
+                .jsonPath();
+        status = getResult.get("status");
+        String resultJson = getResult.get("result");
+        System.out.println("Check status job after delay: "+status);
+        System.out.println("Result = "+resultJson);
 
     }
 }
