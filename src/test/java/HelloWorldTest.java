d890901f3871aa9a363fe0fbab4797f64383f03b
lopuh <G@rut0U2um@k1>
2022-11-16 10:16:48 +0300

    Added ex8 resolution

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -5,45 +5,27 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.time.*;
 
 
 public class HelloWorldTest {
-    static String uri = "https://playground.learnqa.ru/ajax/api/longtime_job";
+    static String uri = "https://playground.learnqa.ru/api/get_json_homework";
     @Test
-    public void testRestAssured() throws InterruptedException {
-        Map<String, String> headers = new HashMap<>();
-
+    public void testRestAssured() {
+        ArrayList<LinkedHashMap<String , String>> messages;
+        Map<String, String> secondMessage;
         JsonPath response = RestAssured
                 .given()
                 .get(uri)
                 .jsonPath();
-        String token = response.get("token");
-        int delay = response.get("seconds");
-        System.out.println("Task will be finished in "+delay+" seconds");
-        headers.put("token", token);
-
-        JsonPath statusCheck = RestAssured
-                .given()
-                .queryParams(headers)
-                .headers(headers)
-                .get(uri)
-                .jsonPath();
-        String status = statusCheck.get("status");
-        System.out.println("Check status Job = "+status);
-        Thread.sleep(delay*1000);
-
-        JsonPath getResult = RestAssured
-                .given()
-                .queryParams(headers)
-                .get(uri)
-                .jsonPath();
-        status = getResult.get("status");
-        String resultJson = getResult.get("result");
-        System.out.println("Check status job after delay: "+status);
-        System.out.println("Result = "+resultJson);
+        messages=response.get("messages");
+        secondMessage=messages.get(1);
+        String finalMessage = secondMessage.get("message");
+        System.out.println(finalMessage);
 
     }
 }
