d7ee6674c8f2c3797e0b4b101afb4527d0978296
lopuh <G@rut0U2um@k1>
2022-12-03 17:02:46 +0300

    Added ex13 resolution

---
+++ src/test/java/Ex13.java
@@ -0,0 +1,28 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class Ex13 {
+    String uri = "https://playground.learnqa.ru/ajax/api/user_agent_check";
+    @ParameterizedTest
+    @CsvSource({
+            "'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30', Mobile, No, Android",
+            "'Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1', Mobile, Chrome, iOS",
+            "'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)', Googlebot, unknown, unknown",
+            "'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0', Web, Chrome, No",
+            "'Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1', Mobile, No, iPhone"
+    })
+    public void ex13(String userAgent, String platform, String browser, String device) {
+        Response response = RestAssured
+                .given()
+                .header("user-agent", userAgent)
+                .get(uri)
+                .andReturn();
+        assertEquals(platform, response.jsonPath().getString("platform"));
+        assertEquals(browser, response.jsonPath().getString("browser"));
+        assertEquals(device, response.jsonPath().getString("device"));
+    }
+}
